c9a30d80acc0e4022944715283c7e417071e9fb0
Valentino Slavkin <robot.slavkin@gmail.com>
2023-10-02 18:32:58 -0300

    Solved time error, tidied code

--- estacionamiento.ino
+++ estacionamiento.ino
@@ -25,26 +25,18 @@ int leerBotones(){
   return 0;
 }
 
-void entrada(int *contador, Servo servoEntrada){
+void entrada(int *contador){
   if(*contador<maxSpace){
     Serial.println("entrada");
     *contador+=1;
-    /* subirBarrera(servoEntrada); */
   }
 }
 
-void salida(int *contador, Servo servoSalida){
+void salida(int *contador){
   if(*contador>0){
     Serial.println("salida");
-    /* subirBarrera(servoSalida); */
     *contador-=1;
   }
-}
-
-void subirBarrera(Servo servo){
-  servo.write(90);
-  delay(2000);
-  servo.write(0);
 }
 
 void imprimirCartel(int libre,int ocupado, LiquidCrystal lcd){
@@ -56,46 +48,6 @@ void imprimirCartel(int libre,int ocupado, LiquidCrystal lcd){
     lcd.print (ocupado);
 }
 
-Servo servoEntrada;
-Servo servoSalida;
-
-LiquidCrystal lcd(8,9,4,5,6,7);
-
-unsigned int contador;
-unsigned long long tiempoServo[]={0,0};
-
-void setup(){
-  lcd.begin(16,2);
-  servoEntrada.attach(pinServoEntrada);
-  servoSalida.attach(pinServoSalida);
-  pinMode(btEntrada, INPUT_PULLUP);
-  pinMode(btSalida, INPUT_PULLUP);
-  pinMode(ledVerde, OUTPUT);
-  pinMode(ledRojo , OUTPUT);
-  Serial.begin(115200);
-}
-
-void loop(){
-  switch(leerBotones()){
-  case 1:
-    if(millis() > tiempoServo[0]+servoDelayTime){
-      tiempoServo[0]=millis();
-      entrada(&contador, servoEntrada);
-    }
-    break;
-  case 2:
-    if(millis() > tiempoServo[1]+servoDelayTime){
-      tiempoServo[1]=millis();
-      salida(&contador, servoSalida);
-    }
-    break;
-  }
-  setearServos(tiempoServo[0], servoEntrada);
-  setearServos(tiempoServo[1], servoSalida);
-  setSemaforo(contador<maxSpace);
-  imprimirCartel(maxSpace-contador,contador, lcd);
-}
-
 void setearServos(unsigned long long tiempoServo, Servo servo){
   if(tiempoServo!=0 && millis() < tiempoServo+servoDelayTime){
     servo.write(90);
@@ -105,3 +57,52 @@ void setearServos(unsigned long long tiempoServo, Servo servo){
   }
 }
 
+//Instancias de los objetos de servo
+Servo servoEntrada;
+Servo servoSalida;
+//Instancia del objeto Liquidcrystal(Para el lcd)
+LiquidCrystal lcd(8,9,4,5,6,7);
+//Contador lleva la cuenta de cuantos autos hay en el estacionamiento
+unsigned int contador;
+//tiempoServo guarda cuando se toco el boton, para poder usar millis() y no bloquear el programa
+unsigned long long tiempoServo[]={0,0};
+
+void setup(){
+  //Inicio el lcd con 16 columnas y 2 filas
+  lcd.begin(16,2);
+  //Inicia los servos en los GPIO correspondientes
+  servoEntrada.attach(pinServoEntrada);
+  servoSalida.attach(pinServoSalida);
+  //Utilizo INPUT_PULLUP para los botones
+  pinMode(btEntrada, INPUT_PULLUP);
+  pinMode(btSalida, INPUT_PULLUP);
+  //Pongo como salidas los GPIOs de los leds
+  pinMode(ledVerde, OUTPUT);
+  pinMode(ledRojo , OUTPUT);
+  
+  Serial.begin(115200);
+}
+
+void loop(){
+  switch(leerBotones()){
+  case 1:
+    if(millis() > tiempoServo[0]+servoDelayTime){
+      tiempoServo[0]=millis();
+      entrada(&contador);
+    }
+    break;
+  case 2:
+    if(millis() > tiempoServo[1]+servoDelayTime){
+      tiempoServo[1]=millis();
+      salida(&contador);
+    }
+    break;
+  }
+  //Escribo ambos servos en base a tiemposervo
+  setearServos(tiempoServo[0], servoEntrada);
+  setearServos(tiempoServo[1], servoSalida);
+  //seteo los leds
+  setSemaforo(contador<maxSpace);
+  //Imprimo el numero de lugares libres y ocupados en el lcd
+  imprimirCartel(maxSpace-contador,contador, lcd);
+}
